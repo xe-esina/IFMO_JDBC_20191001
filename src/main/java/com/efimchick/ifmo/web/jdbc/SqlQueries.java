413ca69eda41fad831e7eececced73907aea35df
Evgenii Efimchik <thejerome@mail.ru>
2019-10-07 16:20:08 +0300

    task-1 exercises

---
+++ src/main/java/com/efimchick/ifmo/web/jdbc/SqlQueries.java
@@ -0,0 +1,54 @@
+package com.efimchick.ifmo.web.jdbc;
+
+/**
+ * Implement sql queries like described
+ */
+public class SqlQueries {
+    //Select all employees sorted by last name in ascending order
+    //language=HSQLDB
+    String select01 = null;
+
+    //Select employees having no more than 5 characters in last name sorted by last name in ascending order
+    //language=HSQLDB
+    String select02 = null;
+
+    //Select employees having salary no less than 2000 and no more than 3000
+    //language=HSQLDB
+    String select03 = null;
+
+    //Select employees having salary no more than 2000 or no less than 3000
+    //language=HSQLDB
+    String select04 = null;
+
+    //Select employees assigned to a department and corresponding department name
+    //language=HSQLDB
+    String select05 = null;
+
+    //Select all employees and corresponding department name if there is one.
+    //Name column containing name of the department "depname".
+    //language=HSQLDB
+    String select06 = null;
+
+    //Select total salary pf all employees. Name it "total".
+    //language=HSQLDB
+    String select07 = null;
+
+    //Select all departments and amount of employees assigned per department
+    //Name column containing name of the department "depname".
+    //Name column containing employee amount "staff_size".
+    //language=HSQLDB
+    String select08 = null;
+
+    //Select all departments and values of total and average salary per department
+    //Name column containing name of the department "depname".
+    //language=HSQLDB
+    String select09 = null;
+
+    //Select all employees and their managers if there is one.
+    //Name column containing employee lastname "employee".
+    //Name column containing manager lastname "manager".
+    //language=HSQLDB
+    String select10 = null;
+
+
+}

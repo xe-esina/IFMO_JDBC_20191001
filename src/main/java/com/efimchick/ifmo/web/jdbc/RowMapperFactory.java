829b09ae7eb22cad989c8d4a928958953a150bc3
Evgenii Efimchik <thejerome@mail.ru>
2019-10-13 18:40:13 +0300

    task-2 exercise

---
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -0,0 +1,10 @@
+package com.efimchick.ifmo.web.jdbc;
+
+import com.efimchick.ifmo.web.jdbc.domain.Employee;
+
+public class RowMapperFactory {
+
+    public RowMapper<Employee> employeeRowMapper() {
+        throw new UnsupportedOperationException();
+    }
+}

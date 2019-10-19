5824521a0e0a98cbd2b6657f0e99735312e2acb6
Evgenii Efimchik <thejerome@mail.ru>
2019-10-19 21:40:05 +0300

    task-5 dep Service is removed from factory

--- src/main/java/com/efimchick/ifmo/web/jdbc/service/ServiceFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/service/ServiceFactory.java
@@ -2,10 +2,6 @@ package com.efimchick.ifmo.web.jdbc.service;
 
 public class ServiceFactory {
 
-    public DepartmentService departmentService(){
-        throw new UnsupportedOperationException();
-    }
-
     public EmployeeService employeeService(){
         throw new UnsupportedOperationException();
     }
